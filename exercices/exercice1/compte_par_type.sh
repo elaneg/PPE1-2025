2c6e2967ef5a632c0f1357cbbee37bf7192076b6
elaneg <elane.grd@gmail.com>
2025-10-12 14:22:40 +0200

    ajout de compte_par_type.sh

--- exercices/exercice1/compte_par_type.sh
+++ exercices/exercice1/compte_par_type.sh
@@ -1,1 +1,15 @@
-#exercice2-a
+#tp1-ex2a 
+#script  qui compte le nombre d'entités données en arguments pour chaque année 
+
+#!/usr/bin/bash
+
+echo "arguments :$1, $2, $3 "
+
+CHEMIN=$1
+ANNEE=$2
+ENTITE=$3
+
+
+echo "Nombre de $3 en $2 :"
+cat $CHEMIN/$ANNEE/* | grep $ENTITE | wc -l
+
