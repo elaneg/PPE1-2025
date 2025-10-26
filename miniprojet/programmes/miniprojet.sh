4f744db2dc10275df4b246d7ef54ca7424350615
elaneg <elane.grd@gmail.com>
2025-10-26 12:43:48 +0100

    étapes 1, 2 et 2.1

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,4 +1,18 @@
+#!/bin/bash
+
+URL=$1
+if [ "$#" -ne 1 ]; then
+echo "Veuillez rentrer un argument"
+exit 
+fi
+
 while read -r line;
 do
 	echo ${line};
-done < "urls/fr.txt";
+done < "$URL";
+
+
+#on n'utilise pas cat car la commande cat considère les liens comme des fichiers et va essayer de les chercher (et donc va afficher "file not found"),
+# or on veut juste écrire les liens.
+
+
